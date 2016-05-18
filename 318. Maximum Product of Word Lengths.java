fe3fcc759ed93752e8156ae91aa718e35d06e7e4
fortune1992 <1049112656@qq.com>
2016-05-18 20:20:59 +0800

    Update 318. Maximum Product of Word Lengths.java

--- 318. Maximum Product of Word Lengths.java
+++ 318. Maximum Product of Word Lengths.java
@@ -1,3 +1,4 @@
+/*
 Given a string array words, find the maximum value of length(word[i]) * length(word[j]) where the two words do not share common letters. You may assume that each word will contain only lower case letters. If no such two words exist, return 0.
 
 Example 1:
@@ -14,6 +15,7 @@ Example 3:
 Given ["a", "aa", "aaa", "aaaa"]
 Return 0
 No such pair of words.
+*/
 
 public class Solution {
     // 注意:the two words do not share common letters!!!不包含相同字母
