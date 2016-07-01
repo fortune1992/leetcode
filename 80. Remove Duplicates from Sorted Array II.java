22625126809bf2ce3b5b74f900cb1eec85efc8d3
fortune1992 <1049112656@qq.com>
2016-07-01 10:29:10 +0800

    Update 80. Remove Duplicates from Sorted Array II.java

--- 80. Remove Duplicates from Sorted Array II.java
+++ 80. Remove Duplicates from Sorted Array II.java
@@ -5,7 +5,8 @@ What if duplicates are allowed at most twice?
 For example,
 Given sorted array nums = [1,1,1,2,2,3],
 
-Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the new length.
+Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3. 
+It doesn't matter what you leave beyond the new length.
 */
 public class Solution {
     // nums已经排好序
